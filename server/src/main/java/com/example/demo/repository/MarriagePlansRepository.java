05550d390d45bb08a9bea4bd5ad1dc2a1e1acd78
plusdojo2025-konuma <konuma-taiga-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-22 16:41:27 +0900

    repository編集

--- server/src/main/java/com/example/demo/repository/MarriagePlansRepository.java
+++ server/src/main/java/com/example/demo/repository/MarriagePlansRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MarriagePlansRepository extends JpaRepository<T, ID> {
+import com.example.demo.entity.MarriagePlans;
+
+public interface MarriagePlansRepository extends JpaRepository<MarriagePlans, Integer> {
 
 }
