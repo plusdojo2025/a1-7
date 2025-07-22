85f419de002d05fdf75f458a6bce97819762899b
plusdojo2025-matsuyama <matuyama-kouki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-22 11:12:26 +0900

    entity

--- server/src/main/java/com/example/demo/repository/UsersRepository.java
+++ server/src/main/java/com/example/demo/repository/UsersRepository.java
@@ -9,7 +9,7 @@ import com.example.demo.entity.Users;
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 	@Query(value = "SELECT count(*) FROM Users WHERE mail_address=? AND password=?"
 			,nativeQuery = true)
-	Integer getUsers(@Param("mail_address") String mail_address, @Param("password") String password);
+	Integer getUsers(@Param("mailAddress") String mail_address, @Param("password") String password);
 }
 
 
