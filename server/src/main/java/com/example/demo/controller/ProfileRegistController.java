0bab75cd1b9db4df0d457f81050d51be815b52eb
shimano5630 <shimano-tatsuaki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-07-23 09:34:01 +0900

    0722作業分

--- server/src/main/java/com/example/demo/controller/ProfileRegistController.java
+++ server/src/main/java/com/example/demo/controller/ProfileRegistController.java
@@ -1,5 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Users;
+import com.example.demo.repository.UsersRepository;
+
+@Controller
 public class ProfileRegistController {
+	
+	@Autowired
+	private UsersRepository repository;
+	
+	@GetMapping("/home/userRegist")
+	public String userProfileRegist(Model model) {
+		model.addAttribute("user", new Users()); 
+        return "profileRegister";
+	}
+	
+	@PostMapping("/home/userRegist")
+	public String userProfileRegist(@ModelAttribute Users users) {
+		repository.save(users);
+		return "redirect:/home";
+	}
+	
+
 
 }
